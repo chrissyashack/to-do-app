bd68042f25c00cf70a8eb5368eed1ae99fc4946e
Christine Ashack <chrissy.ashack@gmail.com>
2018-07-29 21:41:34 -0500

    assignment 6 completed

--- app.js
+++ app.js
@@ -1,44 +1,68 @@
 function onReady() {
-  const toDos = [];
-  const addToDoForm = document.getElementbyId('addToDoForm');
+  let toDos = JSON.parse( localStorage.getItem('toDos') ) || [];
+  let id = toDos.length || 0;
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
 
   function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) {return}
-
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: ++id
     });
-    newToDoText.value = '';
+  }
 
-    renderTheUI();
+  function deleteToDo(id) {
+    return toDos.filter(toDo => toDo.id !== id);
+  }
+
+  function saveToDos() {
+    localStorage.setItem('toDos', JSON.stringify(toDos) );
   }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
+
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+      checkbox.checked = toDo.complete;
 
-      newLi.textcontent = toDo.title;
+      const deleteBtn = document.createElement('button')
+      deleteBtn.innerHTML = '<span>Delete</span>';
+
+      newLi.innerHTML = toDo.title;
+
+      checkbox.addEventListener('click', function() {
+        toDo.complete = checkbox.checked ? true : false;
+        saveToDos();
+      })
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      newLi.appendChild(deleteBtn);
 
+      deleteBtn.addEventListener('click', () => {
+        toDos = deleteToDo(toDo.id);
+        renderTheUI();
+        saveToDos();
+      });
+    });
   }
 
-  addToDoForm.addEventListener('submit', event => {
+  addToDoForm.addEventListener('submit', function(event) {
     event.preventDefault();
     createNewToDo();
+    newToDoText.value = '';
+    renderTheUI();
+    saveToDos();
   });
 
   renderTheUI();
+  saveToDos();
 }
 
 window.onload = function() {
