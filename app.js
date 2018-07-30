966820ad12fea44d419703547060757745fdae46
Christine Ashack <chrissy.ashack@gmail.com>
2018-07-29 21:16:56 -0500

    Assignment 5 completed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
+  const toDoList = document.getElementById('toDoList');
   const newToDoText = document.getElementById('newToDoText');
-  const ToDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
+  addToDoForm.addEventListener ('submit', function(event) {
     event.preventDefault();
 
-    //get the newToDoText
+    //get the text
     let title = newToDoText.value;
 
     //create a new li
@@ -15,24 +15,35 @@ function onReady() {
     //create a new input
     let checkbox = document.createElement('input');
 
-    //set the input's type to checkbox
+    //create a delete delete button
+    let deleteButton = document.createElement('button')
+
+    deleteButton.innerHTML = 'delete';
+
+    //checkbox
     checkbox.type = "checkbox";
 
     //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
+    newLi.innerHTML = title;
 
     //attach the li to the ul
     toDoList.appendChild(newLi);
 
+    //attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+    // attach deleteButton to the li
+    newLi.appendChild(deleteButton);
+
     //empty the input
     newToDoText.value = '';
 
+    deleteButton.addEventListener = function() {
+      newLi.parentNode.removeChild(newLi);
+      }
   });
 }
 
 window.onload = function() {
   onReady();
-};
+}
